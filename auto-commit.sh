603d57086b4f1602a0ff5c60ec0ca0bb8986aa2c
tebellomaila <tebellomakhura@outlook.com>
2024-11-17 16:35:27 +0000

    git commit -m "$(date)"

--- auto-commit.sh
+++ auto-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add . 
-git commit -m "Automated commit: $(date) - Progress saved and pushed to GitHub"
+git commit -m "Automated commit: $(date) - Progress saved"
 git push origin main
 echo "Progress saved and pushed to Git"
 
